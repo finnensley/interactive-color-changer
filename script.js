c943fdb9aeef990705044992a9c1f9d45b672640
finnensley <finniganensley@gmail.com>
2025-06-17 12:35:45 -0600

    added script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const buttons = document.querySelectorAll(".color-btn");
 const resetButton = document.getElementById("reset");
 
-buttons.forEach(button => {
-    button.addEventListner
-})
+buttons.forEach((button) => {
+    button.addEventListener("click", function() {
+        document.body.style.backgroundColor = button.getAttribute("data-color");
+    });
+});
+
+resetButton.addEventListener("click", function() {
+    document.body.style.backgroundColor = "white";
+});
+
+
+
+
